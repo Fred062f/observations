7f9137164f5360c3fdb7f7c45ecfdab490fa12e2
ViktorBach <vmb0107@gmail.com>
2023-11-17 09:32:01 +0100

    edited button code

--- main.js
+++ main.js
@@ -54,7 +54,6 @@ const chart = new Chart(ctx, {
             label: '# of Observations',
             data: counts,
             borderWidth: 1,
-
 }],
     },
     options: {
@@ -156,7 +155,7 @@ const monthlyObservationsBarChart = new Chart(monthlyObservationsCtx, {
 });
 //button
 //get button
-let topButton = document.getElementById("myBtn");
+let topButton = document.querySelector("#top-button");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
@@ -170,7 +169,7 @@ function scrollFunction() {
 }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+topButton.addEventListener("click", (event) => {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+})
